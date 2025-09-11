3aa2d439d27f2b8ceb7e31297442c86a486a75e2
VargaO <2006vargaoliver@gmail.com>
2025-09-11 15:09:34 +0200

    commit

--- feladat2025.09.11/Program.cs
+++ feladat2025.09.11/Program.cs
@@ -14,15 +14,22 @@ namespace alap
 
             Console.WriteLine("adja meg a nevét: ");
             string nev = Console.ReadLine();
+            Console.WriteLine("2. Üdvözlöm " + nev);
+
 
             Console.WriteLine("adjon meg egy számot: ");
             int szam = int.Parse(Console.ReadLine());
+            Console.WriteLine("3. a szám kétszerese: " + szam * 2);
 
-            asdsa
+
+            Console.WriteLine("adja meg az első száámot: ");
+            int szam1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("adja meg a második száámot: ");
+            int szam2 = int.Parse(Console.ReadLine());
 
 
-            Console.WriteLine("2. Üdvözlöm " + nev);
-            Console.WriteLine("3. a szám kétszerese: " + szam*2);
+            Console.WriteLine($"a két szám összege: {szam1 + szam2}, a két szám különbsége: {szam1 - szam2},");
 
 
         }
